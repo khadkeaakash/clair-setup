f3f8ae46abc331057147281636df77ef0c191328
khadkeaakash <khadke_aakash@yahoo.co.in>
2020-07-18 16:31:04 +0800

    Update setupclair.groovy

--- setupclair.groovy
+++ setupclair.groovy
@@ -41,7 +41,7 @@ pipeline {
 {
   "auths":{
     "quay.io": {
-        "auth": "${quay-auth-token}",
+        "auth": ${quay-auth-token},
         "email": ""
     }
   }
@@ -51,7 +51,7 @@ EOF
                                         cat config.json
                                         cd $WORKSPACE
                                         
-                                        docker login -u="coreos+rhcp" -p="${redhat-quay-token}" quay.io
+                                        docker login -u "coreos+rhcp" -p ${redhat-quay-token} quay.io
                                         docker pull quay.io/quay/redis
                                         docker run --rm --name quayredisdb -d -p 6379:6379 quay.io/quay/redis
                                         docker pull quay.io/coreos/quay:v2.9.3
