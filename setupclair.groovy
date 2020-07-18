0b8b8a48672b6d527fe93a10fbbc9bf8f25c9ae3
khadkeaakash <khadke_aakash@yahoo.co.in>
2020-07-18 14:22:52 +0800

    Update setupclair.groovy

--- setupclair.groovy
+++ setupclair.groovy
@@ -7,8 +7,8 @@ pipeline {
             steps {
                 sh '''
                 docker pull postgres:latest
-                docker run --name postgres -p 5432:5432 -d postgres
-                docker run --rm --link postgres:postgres postgres sh -c 'echo "create database clairtest" | psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres'
+                docker run --name postgres -e POSTGRES_PASSWORD=chaklee -d postgres
+                #docker run --rm --link postgres:postgres postgres sh -c 'echo "create database clairtest" | psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres'
                 '''
             } //end of steps
         } //end of stage build
